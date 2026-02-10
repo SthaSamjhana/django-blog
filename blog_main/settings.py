a099fd33ee781f0085f54e8937746a23a6279580
Samtha Stha <samshrestha990@gmail.com>
2026-02-10 18:30:09 +0545

    blogs app and blog model

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs',
     
 ]
 
@@ -120,3 +121,5 @@ STATICFILES_DIRS = [
     BASE_DIR / 'blog_main/static',
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
